b2553f1c0e0b8e2aa4304ca0cfdb3f4c86ab1766
Billones142 <stefanomerinoderui@gmail.com>
2024-11-25 10:37:17 -0300

    pragma once

--- lib/ota/otaLib.h
+++ lib/ota/otaLib.h
@@ -1,9 +1,6 @@
+#pragma once
 #include <Arduino.h>
 
-#ifndef OTA_LIB
-#define OTA_LIB
 
 void OTA_Init(const char* password, uint16_t port);
 void arduino_OTA_Handle();
-
-#endif
--- lib/wifi/wifiLib.h
+++ lib/wifi/wifiLib.h
@@ -1,3 +1,5 @@
+#pragma once
+
 void wifi_Init(const char* ssid, const char* password);
 
 void wifi_print_status();
