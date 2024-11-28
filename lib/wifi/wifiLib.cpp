73ce7cc62a299b36eb6c948741e4bcab2a5ef139
Billones142 <stefanomerinoderui@gmail.com>
2024-11-28 11:36:03 -0300

    simplified messages

--- lib/wifi/wifiLib.cpp
+++ lib/wifi/wifiLib.cpp
@@ -14,24 +14,18 @@ void wifi_Init(const char* ssid, const char* password){
     while (wifiStatus != WL_CONNECTED) {
         wifiStatus = WiFi.status();
         delay(1000);
-        String defaultMessage= "Connecting to WiFi... Code= ";
-        String mensaje= defaultMessage + wifiStatus;
-        Serial.println(mensaje);
+        Serial.println((String)"Connecting to WiFi... Code= " + wifiStatus);
     }
     Serial.println("Connected to WiFi");
 }
 
 void wifi_print_status() {
     // print the SSID of the network you're attached to:
-    String message_ssid= (String)"SSID: " + WiFi.SSID();
-    Serial.println(message_ssid);
+    Serial.println((String)"SSID: " + WiFi.SSID());
        
     // print your WiFi shield's IP address:
-    String message_ip_address= (String)"IP Address: " + WiFi.localIP();
-    Serial.println(message_ip_address);
+    Serial.println((String)"IP Address: " + WiFi.localIP());
 
     // print the received signal strength:
-
-    String message_signal_strenght= (String)"signal strength (RSSI): " + WiFi.RSSI() + " dBm";
-    Serial.println(message_signal_strenght);
+    Serial.println((String)"signal strength (RSSI): " + WiFi.RSSI() + " dBm");
 }
