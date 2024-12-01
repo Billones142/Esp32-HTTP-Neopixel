2256059601f1c2d19ab93e96936136ab08be14ac
Billones142 <stefanomerinoderui@gmail.com>
2024-12-01 00:38:48 -0300

    Refactor mDNS initialization in setup function
    - Replaced the inclusion of <mdns.h> with <mdnsInit.h> to align with the new mDNS initialization implementation.
    - Moved mDNS initialization to the end of the setup function to ensure it occurs after WiFi configuration.
    - Added a comment to clarify the purpose of disabling the brownout detector for WiFi stability.

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,16 @@
 #include <httpServer.h>
 #include <otaLib.h>
 #include <neopixel.h>
-#include <mdns.h>
+#include <mdnsInit.h>
 
 #pragma message("\nWiFi SSID: " WIFI_SSID "\nWiFi Password: " WIFI_PASSWORD)
 
 void setup()
 {
     Serial.begin(115200U);
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0U);
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0U); // disables brownout detector, fixes wifi problem
 
     wifi_Init(WIFI_SSID, WIFI_PASSWORD);
-    mDNS_Init(WIFI_HOSTNAME, OTA_PORT);
 
     OTA_Init(OTA_PASSWORD, OTA_PORT); // Comes after the wifi config (needs the ip)
 
@@ -25,6 +24,8 @@ void setup()
 
     neopixel_Init(NUMPIXELS, PIN);
     initHttpServer();
+
+    mDNS_Init(WIFI_HOSTNAME, OTA_PORT);
 }
 
 void loop()
