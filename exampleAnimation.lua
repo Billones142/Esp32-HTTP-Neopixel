6218dd364e9b712b8dfd6cfa48e7c8c686473347
Billones142 <stefanomerinoderui@gmail.com>
2024-12-06 20:37:53 -0300

    Refactor exampleAnimation.lua and index.html for improved functionality and UI
    - Updated exampleAnimation.lua to alternate pixel color setting and clearing based on the for index.
    - Modified index.html to enhance textarea styling, including width, background color, and text color.
    - Improved JavaScript handling of script data types for better reliability and clarity in responses.

--- exampleAnimation.lua
+++ exampleAnimation.lua
@@ -1,6 +1,10 @@
 for i= 0, 255 do
-    for index= 0, getPixelAmount()-1 do
-        setPixelColor(index,colorWheel(i))
+    if ((i % 2) == 0) then
+        for index= 0, getPixelAmount()-1 do
+            setPixelColor(index,colorWheel(i))
+        end
+    else
+        clearPixels();
     end
     showPixels()
     delay(20)
