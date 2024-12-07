8fb413174078c43b0e63f5440d0cb801fbd77ec3
Billones142 <stefanomerinoderui@gmail.com>
2024-12-07 15:17:53 -0300

    Refactor HTTP server for enhanced JSON processing and error handling
    - Introduced a new base function for processing JSON requests, improving code reuse and clarity.
    - Enhanced error handling in JSON processing to provide more informative responses.
    - Updated endpoints to return structured JSON responses for pixel count and saved scripts.
    - Removed deprecated functions and streamlined the server's response handling.
    - Improved overall organization and readability of the HTTP server code.

--- lib/httpServer/httpServer.cpp
+++ lib/httpServer/httpServer.cpp
@@ -6,19 +6,33 @@
 #include <neopixel.h>
 
 #define HTML_URL "https://billones142.github.io/Esp32-HTTP-Neopixel/data/index.html"
-// #include <WiFiServerSecure.h>
 
 AsyncWebServer server(80);
-// WiFiServerSecure server(443);
 
-// functions used only inside this lib
-void setResponse(String &message, int jsonResponse);
-void sendJsonResponse(AsyncWebServerRequest *request, const String &status, const String &message);
-
-void sendJsonResponse(AsyncWebServerRequest *request, const String &status, const String &message)
+// typedef std::function<void(AsyncWebServerRequest* request)> JsonProcess;
+typedef std::function<void(Adafruit_NeoPixel &pixelStrip, String jsonString)> JsonProcess;
+void processJsonBase(JsonProcess processJsonFunction, AsyncWebServerRequest *request, String requestData, String succesMessage)
 {
-    String content = "{\"status\":\"" + status + "\",\"message\":\"" + message + "\"}";
-    request->send("Success" ? 200 : 500, "application/json", content);
+    JsonDocument response;
+
+    try
+    {
+        processJsonFunction(pixels, requestData.c_str());
+        response["status"] = "Success";
+        response["message"] = succesMessage;
+    }
+    catch (const std::runtime_error &e)
+    {
+        response["status"] = "Error";
+        response["message"] = e.what();
+    }
+    catch (...)
+    {
+        response["status"] = "Error";
+        response["message"] = "Unknown error";
+    }
+
+    request->send(200, "application/json", response.as<String>());
 }
 
 void initHttpServer()
@@ -36,23 +50,32 @@ void initHttpServer()
     server.on("/*", HTTP_OPTIONS, [](AsyncWebServerRequest *request)
               {
         AsyncWebServerResponse *response = request->beginResponse(204); // No Content
-        response->addHeader("Access-Control-Allow-Origin", "*");
+        /*response->addHeader("Access-Control-Allow-Origin", "*");
         response->addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-        response->addHeader("Access-Control-Allow-Headers", "Content-Type");
+        response->addHeader("Access-Control-Allow-Headers", "Content-Type");*/
         request->send(response); });
 
     // Serve a simple HTML page
     server.on("/getNumpixels", HTTP_GET, [](AsyncWebServerRequest *request)
               {
-        String message= (String)"{\"message\":\"" + pixels.numPixels() + " pixel/s\"}";
-        request->send(200, "text/html", 
-        message.c_str()); });
+        JsonDocument bodyResponse;
+        bodyResponse["message"]= (String)pixels.numPixels() + " pixel/s";
+        request->send(200, "text/html", bodyResponse.as<String>()); });
 
     server.on("/getSavedScripts", HTTP_GET, [](AsyncWebServerRequest *request)
               {
         JsonDocument bodyResponse;
         bodyResponse["status"]= "Success";
-        bodyResponse["scripts"]= getSavedLuaScripts();
+        try
+        {
+            bodyResponse["scripts"]= getSavedLuaScripts(true);
+        }
+        catch(...)
+        {
+            bodyResponse["scripts"]= "failed";
+        }
+        
+
         if (bodyResponse["scripts"].size() > 0)
         {
             bodyResponse["message"]= "Scripts send";
@@ -61,87 +84,20 @@ void initHttpServer()
         {
             bodyResponse["message"]= "No scripts found";
         }
-        
-        
 
-        request->send(200, "application/json", bodyResponse.as<String>());});
+        request->send(200, "application/json", bodyResponse.as<String>()); });
 
     server.on("/setcolor", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
               {
-        String status;
-        String message;
-        
+        stopLuaTask();
         String requestData = String((char*)data, len);
-        
-        int jsonResponse = processJsonToNeopixelStatic(pixels, requestData.c_str());
-
-        if (jsonResponse == NeopixelJsonStatus::JSON_OK)
-        {
-            status= "Success";
-        }
-        else
-        {
-            status= "Error";
-        }
-
-        setResponse(message, jsonResponse);
-       
-        String content= (String)"{\"status\":\"" + status + "\",\"message\":\"" + message + "\"}";
-        sendJsonResponse(request, status, message); });
+        processJsonBase(processJsonToNeopixelStatic, request, requestData, "Static colour applied"); });
 
     server.on("/setScript", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
               {
-        String status;
-        String message;
-        
         String requestData = String((char*)data, len);
-        
-        int jsonResponse = processJsonToNeopixelScript(pixels, requestData.c_str());
-
-        if (jsonResponse == NeopixelJsonStatus::JSON_OK)
-        {
-            status= "Success";
-        }
-        else
-        {
-            status= "Error";
-        }
-
-        setResponse(message, jsonResponse);
-       
-        String content= (String)"{\"status\":\"" + status + "\",\"message\":\"" + message + "\"}";
-        sendJsonResponse(request, status, message); });
+        processJsonBase(processJsonToNeopixelScript, request, requestData, "Script applied"); });
 
     server.begin();
     Serial.println("HTTP server started");
-}
-
-void setResponse(String &message, int jsonResponse)
-{
-    switch (jsonResponse)
-    {
-    case NeopixelJsonStatus::JSON_OK:
-        message = "Neopixel strip updated";
-        break;
-
-    case NeopixelJsonStatus::NO_PROPERTY_COLOURS:
-        message = "Colours property not found in json";
-        break;
-
-    case NeopixelJsonStatus::JSON_PARSE_ERROR:
-        message = "Json parse error";
-        break;
-
-    case NeopixelJsonStatus::COLOURS_ARRAY_EMPTY:
-        message = "Colours array empty in json";
-        break;
-
-    case NeopixelJsonStatus::UKNOWNN_ERROR:
-        message = "Unknown json error";
-        break;
-
-    default:
-        message = "Unknown led status";
-        break;
-    }
 }
